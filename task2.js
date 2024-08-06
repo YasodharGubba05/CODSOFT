430e97ae09619aeff850c1bc2f58eb8e5431d28c
YasodharGubba05 <83855085+YasodharGubba05@users.noreply.github.com>
2024-08-06 21:09:08 +0530

    created task2.js

---
+++ task2.js
@@ -0,0 +1,9 @@
+const linkedin = document.querySelector('#linkedin');
+linkedin.addEventListener("click", function() {
+    window.open("https://www.linkedin.com/in/yasodhar-gubba-18aa84209", "_blank");
+});
+
+const github = document.querySelector('#github');
+github.addEventListener("click", function() {
+    window.open("https://github.com/YasodharGubba05", "_blank");
+});
