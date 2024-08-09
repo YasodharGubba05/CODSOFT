b85d9eaa7713f7770451bc1e99feaa472db586f5
YasodharGubba05 <83855085+YasodharGubba05@users.noreply.github.com>
2024-08-09 15:13:27 +0530

    updated task2.js

--- task2.js
+++ task2.js
@@ -13,18 +13,28 @@ skills.addEventListener('click', function() {
     document.getElementById('section3').scrollIntoView({ behavior: 'smooth' });
   });
 
-  const projects=document.querySelector('#projects');
+const projects=document.querySelector('#projects');
   projects.addEventListener('click', function() {
       document.getElementById('section2').scrollIntoView({ behavior: 'smooth' });
     });
 
-   const about=document.querySelector('#blogs');
+const about=document.querySelector('#blogs');
     about.addEventListener('click', function() {
         document.getElementById('section1').scrollIntoView({ behavior: 'smooth' });
       });
 
-      const details=document.querySelector('#contactme');
+ const details=document.querySelector('#contactme');
       details.addEventListener('click', function() {
           document.getElementById('section4').scrollIntoView({ behavior: 'smooth' });
         });
+
+ const mylinkedin = document.querySelector('#mylinkedin');
+        mylinkedin.addEventListener("click", function() {
+            window.open("https://www.linkedin.com/in/yasodhar-gubba-18aa84209", "_blank");
+        });
+
+        const reachout=document.querySelector('#reachout');
+        reachout.addEventListener('click', function() {
+            document.getElementById('section4').scrollIntoView({ behavior: 'smooth' });
+          });       
 
