4ca1d6f73ea44b688a0df86d9217f3869fc7ccef
YasodharGubba05 <83855085+YasodharGubba05@users.noreply.github.com>
2024-08-10 11:38:48 +0530

    Final task3.js

--- task3.js
+++ task3.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const inputField = document.getElementById('input');
-    const buttons = document.querySelectorAll('.button');
+ const inputField = document.getElementById('input');
+ const buttons = document.querySelectorAll('.button');
 
     buttons.forEach(button => {
         button.addEventListener('click', () => {
